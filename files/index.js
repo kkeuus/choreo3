d4bb2b2b5dd21667190bad963e9d2fba89a0e1bb
kkeuus <WalshToni@protonmail.com>
2024-08-27 01:26:40 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 6000;        
-const UUID = process.env.UUID || 'f9d32261-d16b-4b44-b4de-e51ba78a7376'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '8998c690-2e5e-401e-b825-304183f6c356'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo22.trefvuikoi8.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWZmYWU1NmJkNzA2MmIzMGQ1Y2RiYmRhMjNhNzE0YzQiLCJ0IjoiN2M4M2M0ZDAtMTVmMC00M2RkLThmZGEtMDMyYjA4N2JkMWZiIiwicyI6Ik9EWXdaREUxWWpjdE1qQmpaQzAwWm1SakxUbGlPV1F0TTJObVltVXhPR1JqTkRjMiJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'chore02.greenjade.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWZmYWU1NmJkNzA2MmIzMGQ1Y2RiYmRhMjNhNzE0YzQiLCJ0IjoiM2MzNDUzNjgtOGRmMC00ODlhLWEzYjAtNTUzMTEzODcyMjk0IiwicyI6Ik1EWmlaR1l3WWpVdE9XUTRZUzAwWm1ZeExUazBOMlF0TldWbU4yWXpPRGRoWmpRMiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
